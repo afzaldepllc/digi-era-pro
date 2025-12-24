f30351e3c1767e227eaa6adc7ad27a5e6223d476
afzaldepllc <afzal.depllc@gmail.com>
2025-12-25 01:56:32 +0500

    export issue

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,6 +5,14 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+
+export function formatCurrency(amount: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(amount)
+}
+
 export function getInitials(name: string): string {
   return name
     .split(' ')
